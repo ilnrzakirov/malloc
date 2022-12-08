80e285e202e71fc488b3d0a14d06b09b35e6bf18
Ilnur <ilnrzakirov@gmail.com>
2022-12-08 18:27:35 +0300

    add vertual memory

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "malloc.h"
 
 
 int main(int argc, char **argv){
--- malloc.h
+++ malloc.h
@@ -2,12 +2,30 @@
 # define MALLOC_H
 
 # include <unistd.h>
-
 # include <sys/mman.h>
 # include <sys/types.h>
-
+# include <string.h>
+# include <stdint.h>
 # include <pthread.h>
+# include <assert.h>
 
+# define u8 uint8_t
+# define u16 uint16_t
+# define STACK_SIZE 32
+# define HEAP_SIZE STACK_SIZE * 4
+# define HEADER 4
+
+typedef struct virtual_memory {
+    u8 stack[STACK_SIZE];
+    char **unmapped;
+    u8 heap[HEAP_SIZE];
+
+    struct {
+        char **data;
+        char **bss;
+        char *text;
+    } data_t;
+}   virtual_memory_t;
 
 
 #endif
